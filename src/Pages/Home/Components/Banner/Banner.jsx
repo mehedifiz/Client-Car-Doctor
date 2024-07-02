454fc58e05626086f679252e9077f66d38471c13
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-07-02 13:02:28 +0600

    banner

--- src/Pages/Home/Components/Banner/Banner.jsx
+++ src/Pages/Home/Components/Banner/Banner.jsx
@@ -10,52 +10,86 @@ const Banner = () => {
     return (
         <div className="carousel w-full h-[600px] object-cover rounded-2xl ">
 
-<div className='absolute h-full bg-gradient-to-r from-[#151515] to-[rgba(21 , 21 , 21 , 0)] '></div>
+
 
 
         <div id="slide1" className="carousel-item relative w-full">
+        <div className="hero-overlay opacity-80  absolute bg-gradient-to-r from-[#151515] to-[rgba(21 , 21 , 21 , 0)]  "></div>
           <img
             src={img1}
             className="w-full" />
-          <div className="absolute h-full left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
+           <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
             <a href="#slide4" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
           <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0  ">
             
-            <div className='text-white space-y-4 w-2/3 '>
-                <h2 className='font-bold text-6xl'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ipsa.</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tempora quia voluptatem tenetur itaque quo excepturi, delectus similique deleniti sint.</p>
-                <button className='btn btn-primary'>Discover More</button>
+            <div className='text-white space-y-4 w-1/3 pl-12 '>
+                <h2 className='font-bold text-6xl'> Affordable Price For Car Servicing</h2>
+                <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                <button className='btn  btn-primary mr-6'>Discover More</button>
                 <button className='btn btn-outline btn-secondary'>Latest Project</button>
             </div>
           </div>
         </div>
+        
         <div id="slide2" className="carousel-item relative w-full">
+        <div className="hero-overlay opacity-80 absolute bg-gradient-to-r from-[#151515] to-[rgba(21 , 21 , 21 , 0)]  "></div>
           <img
             src={img2}
             className="w-full" />
-          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
+           <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
             <a href="#slide1" className="btn btn-circle">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
           </div>
+          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0  ">
+            
+            <div className='text-white space-y-4 w-1/3 pl-12 '>
+                <h2 className='font-bold text-6xl'> Affordable Price For Car Servicing</h2>
+                <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                <button className='btn btn-primary mr-6'>Discover More</button>
+                <button className='btn btn-outline btn-secondary'>Latest Project</button>
+            </div>
+          </div>
         </div>
+        
         <div id="slide3" className="carousel-item relative w-full">
+        <div className="hero-overlay opacity-80 absolute bg-gradient-to-r from-[#151515] to-[rgba(21 , 21 , 21 , 0)]  "></div>
           <img
             src={img3}
             className="w-full" />
-          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
+           <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
             <a href="#slide2" className="btn btn-circle">❮</a>
             <a href="#slide4" className="btn btn-circle">❯</a>
           </div>
+          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0  ">
+            
+            <div className='text-white space-y-4 w-1/3 pl-12 '>
+                <h2 className='font-bold text-6xl'> Affordable Price For Car Servicing</h2>
+                <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                <button className='btn btn-primary mr-6'>Discover More</button>
+                <button className='btn btn-outline btn-secondary'>Latest Project</button>
+            </div>
+          </div>
         </div>
+       
         <div id="slide4" className="carousel-item relative w-full">
+        <div className="hero-overlay opacity-80 absolute bg-gradient-to-r from-[#151515] to-[rgba(21 , 21 , 21 , 0)]  "></div>
           <img
             src={img4}
             className="w-full" />
-          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
+           <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0 justify-end gap-10">
             <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+          <div className="absolute left-5 right-5 flex -translate-y-1/2 transform bottom-0  ">
+            
+            <div className='text-white space-y-4 w-1/3 pl-12 '>
+                <h2 className='font-bold text-6xl'> Affordable Price For Car Servicing</h2>
+                <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                <button className='btn btn-primary mr-6'>Discover More</button>
+                <button className='btn btn-outline btn-secondary'>Latest Project</button>
+            </div>
           </div>
         </div>
       </div>
