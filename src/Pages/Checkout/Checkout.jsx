1915c779305e8133c1a25e3e23c077a78eeed764
mehedifiz <mehedi.shanto.contract@gamil.com>
2024-07-03 21:08:59 +0600

    Create Order page UI and Take  order

---
+++ src/Pages/Checkout/Checkout.jsx
@@ -0,0 +1,108 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { Authcontext } from "../../providers/Authprovider";
+
+const Checkout = () => {
+
+    const {user} = useContext(Authcontext)
+
+    const service = useLoaderData()
+    const { title , price , _id, img} =service
+
+    const handleBook = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = user?.email
+        
+        const order = {
+            name,
+            date ,
+            email,
+            price: price,
+            service : title,
+            service_id : _id,
+            
+
+        }
+
+        console.log(order)
+       
+
+        fetch('http://localhost:5000/bookings',{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json'
+            }
+            ,
+            body: JSON.stringify(order)
+        }
+        ) 
+
+
+        .then(res => res.json() )
+        .then(data =>{
+            console.log(data)
+        })
+
+        .catch(e =>{
+            console.log(e)
+        })
+
+ }
+    
+    return (
+
+       
+        
+        <div>
+
+            <h2 className="text-center text-3xl font-semibold">Book Service :{title}</h2>
+
+
+      <form onSubmit={handleBook} className="card-body">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name="name" placeholder="Name" defaultValue={user?.name} className="input input-bordered" required />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Date</span>
+          </label>
+          <input type="date" name="date" className="input input-bordered" required />
+          
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Due amount</span>
+          </label>
+          <input type="text" defaultValue={"$" +price} className="input input-bordered" required />
+          
+        </div>
+       
+        </div>
+        <div className="form-control mt-6">
+          <input className="btn btn-block bg-orange-500 text-white  hover:text-xl hover:bg-orange-700" type="submit" value="Order Comfrim" />
+        </div>
+      </form>
+    </div>
+  
+            
+        
+    );
+};
+
+export default Checkout;
--- src/Pages/Home/Components/Service/Service.jsx
+++ src/Pages/Home/Components/Service/Service.jsx
@@ -1,7 +1,8 @@
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Service = ({service}) => {
-  const {title , img , price} = service;
+  const {_id ,title , img , price} = service;
     return (
         <div className="card bg-base-100 w-96 sm:w-full shadow-md">
   <figure className="px-10 pt-10">
@@ -14,7 +15,7 @@ const Service = ({service}) => {
     <h2 className="card-title">{title}</h2>
     <div className="card-actions items-center">
     <p className="text-xl text-orange-600 font-bold"> Price :${price}</p>
-    <IoIosArrowRoundForward className="text-4xl" />
+   <Link to={`/checkout/${_id}`}> <IoIosArrowRoundForward className="text-4xl" /></Link>
     </div>
   </div>
 </div>
