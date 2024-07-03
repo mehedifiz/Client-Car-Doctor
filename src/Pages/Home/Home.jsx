b121897f2c829154e97feb0df7090878d473747b
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-07-03 11:04:41 +0600

    Why os

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import About from "./Components/Aboute/About";
 import Banner from "./Components/Banner/Banner";
 import Services from "./Components/Service/Services";
 import Team from "./Components/Team/Team";
+import WhyUs from "./Components/WhyUs/WhyUs";
 
 
 const Home = () => {
@@ -21,10 +22,12 @@ const Home = () => {
 
 
 
+
 {/* our team */}
 
 <Team></Team>
          
+<WhyUs></WhyUs>
             
         </div>
     );
